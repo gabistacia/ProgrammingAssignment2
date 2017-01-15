d1d421a044b1dd5708466bc989a6d294e002dda4
gabistacia <gabriela.morales.diaz@gmail.com>
2017-01-14 20:28:36 -0500

    adding makeCacheMatrix and cacheSolve functions for Programing assigment2 of Coursera DataSCiente Course 2  - R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions allows you to catch the inverse of a matrix
+## if it was calculated before instead of doing the calculation everytime
 
-## Write a short comment describing this function
+## Creates an object with setters and getters for a matrix and it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s<-NULL
+  set <-function(y){
+    x<<-y
+    s<<-NULL
+  }
+  get<-function() x
+  setInverse<-function(xinverse) s <<- xinverse
+  getInverse<-function() s
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a matrix, if teh inverse was calculated before then
+## it returns the cached date instead of computing the calculation again
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getInverse()
+  if(!is.null(s)){
+    message("getting cached data")
+    return(s)
+  }
+  matrixdata <- x$get()
+  s <- solve(matrixdata,...)
+  x$setInverse(s)
+  s
 }
